a00f4219b2488144f794876a41b7cc5b35298f9a
Obikoya Adebayo <obikoyaadebayo55@gmail.com>
2023-11-26 00:38:22 +0100

    Close feature: to close other accordions when one is opened

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const options = document.querySelectorAll(".option-container");
 
 options.forEach((options) => {
     options.addEventListener("click", () => {
+        const active = document.querySelector(".option-container.active");
+        active.classList.remove("active");
         options.classList.toggle("active");
     })
 })
